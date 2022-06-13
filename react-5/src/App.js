8527d1b14d798dd9d83115ff395f2d7d3b40d6a3
Sandhiya Gopal <88146172+Sandhiyagopal@users.noreply.github.com>
2022-06-13 20:48:55 +0530

    add

--- react-5/src/App.js
+++ react-5/src/App.js
@@ -1,6 +1,7 @@
 import Message1 from './Message1';
 import Navbar from './Navbar/Navbar';
 import Msg from './Msg'
+import State1 from './State/State1';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Hello World</h1>
       <Message1 />
       <hr />
+      <State1 />
       <Msg />
     </div>
   );
