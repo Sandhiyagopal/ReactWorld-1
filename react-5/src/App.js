560bdb4fb1bfd2058f8bc3102cd62556ed8bc1f5
Sandhiya Gopal <88146172+Sandhiyagopal@users.noreply.github.com>
2022-06-13 21:00:34 +0530

    ad

--- react-5/src/App.js
+++ react-5/src/App.js
@@ -1,9 +1,6 @@
 import Message1 from './Message1';
 import Navbar from './Navbar/Navbar';
 import Msg from './Msg'
-import State1 from './State/State1';
-import User from './Props/User';
-import Product from './Product/Product';
 
 function App() {
   return (
