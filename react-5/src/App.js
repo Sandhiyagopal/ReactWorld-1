ac9a6f7b05de89da8fd4a4134ed1dc10112064f6
Sandhiya Gopal <88146172+Sandhiyagopal@users.noreply.github.com>
2022-06-24 12:59:53 +0530

    add

--- react-5/src/App.js
+++ react-5/src/App.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react'
-import Navbar1 from './Navbar/Navbar1'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Nav/Home'
-import About from './Nav/About'
-import Contact from './Nav/Contact'
-import Users from './User/Users'
-import Hook1 from './Hook/Hook1'
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-        <Navbar1 />
-        <Routes>
-          <Route path="/users" element={<Users />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />}/>
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/hook1" element={<Hook1 />} />?
-        </Routes>
-        </Router>
         <h2>App data</h2>
       </div>
     )
